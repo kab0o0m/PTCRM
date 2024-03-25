2dd95ab47a81a61986addfb379b132419fd7546a
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-03-25 18:15:01 +0800

    changed login to just email and password

--- crm/account/models.py
+++ crm/account/models.py
@@ -4,10 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = None
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
